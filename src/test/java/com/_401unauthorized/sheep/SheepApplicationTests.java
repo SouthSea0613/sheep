508351ba50af8032627e5b3010d53890aef7a0c2
South_Sea13 <tksz0613@gmail.com>
2025-02-06 14:12:30 +0900

    default setting

---
+++ src/test/java/com/_401unauthorized/sheep/SheepApplicationTests.java
@@ -0,0 +1,13 @@
+package com._401unauthorized.sheep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
