4566fb30c08b60d5c9cf190d0908a9fbef6b8eff
South_Sea13 <tksz0613@gmail.com>
2025-02-07 15:48:13 +0900

    Add Dto

---
+++ src/main/java/com/_401unauthorized/sheep/dto/WishDto.java
@@ -0,0 +1,15 @@
+package com._401unauthorized.sheep.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Accessors(chain = true)
+public class WishDto {
+}
