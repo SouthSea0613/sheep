9b42110cde1dc14100c7099fac24df7c59328d22
South_Sea13 <tksz0613@gmail.com>
2025-02-10 17:14:48 +0900

    update

--- src/main/java/com/_401unauthorized/sheep/dto/UserDto.java
+++ src/main/java/com/_401unauthorized/sheep/dto/UserDto.java
@@ -13,6 +13,10 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class UserDto {
     String user_id;
+    String user_pw;
     String user_name;
     String user_phone;
+    String user_addr;
+    String user_email;
+    String user_type;
 }
