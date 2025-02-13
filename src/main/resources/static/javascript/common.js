07fe0d51d260be5e1d0dbcf99e4b24447892a2ff
eunhye0123 <kimun569@gmail.com>
2025-02-13 14:07:44 +0900

    헤더 적용 완료

--- src/main/resources/static/javascript/common.js
+++ src/main/resources/static/javascript/common.js
@@ -1,6 +1,6 @@
 function loginStatus() {
     if (user) {
-        $('#user_name').html(`${user.user_id}님`)
+        $('#user_name').html(`${user}님`)
         $('.success').css('display','block');
         $('.before').css('display', 'none');
     } else {
