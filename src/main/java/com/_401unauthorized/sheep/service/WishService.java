c6db7a97368750c0f7f3d0339c1f8e5ab81b5738
eunhye0123 <kimun569@gmail.com>
2025-02-18 16:55:04 +0900

    커밋 1

--- src/main/java/com/_401unauthorized/sheep/service/WishService.java
+++ src/main/java/com/_401unauthorized/sheep/service/WishService.java
@@ -1,13 +1,23 @@
 package com._401unauthorized.sheep.service;
 
 import com._401unauthorized.sheep.dao.WishDao;
+import com._401unauthorized.sheep.dto.WishDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class WishService {
     private final WishDao wishDao;
+    public String write(HttpSession httpSession, Model model) {
+
+    }
 }
