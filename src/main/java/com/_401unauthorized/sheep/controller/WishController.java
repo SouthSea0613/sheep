6b1308a44d064430aebde148af6d9d90380aa51a
alsdud0301 <jm931017@gmail.com>
2025-02-18 16:55:20 +0900

    위시리스트 기능 작성중

--- src/main/java/com/_401unauthorized/sheep/controller/WishController.java
+++ src/main/java/com/_401unauthorized/sheep/controller/WishController.java
@@ -1,9 +1,13 @@
 package com._401unauthorized.sheep.controller;
 
+import com._401unauthorized.sheep.dto.WishDto;
 import com._401unauthorized.sheep.service.WishService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -12,4 +16,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WishController {
     private final WishService wishService;
+
+    @GetMapping("/list")
+    public String wish(){
+
+        return "wish/list";
+    }
+    @GetMapping("/write")
+    public String write(){
+        return "wish/write";
+    }
+    @PostMapping("/write")
+    public String wish(HttpSession session, WishDto wishDto){
+
+        return "";
+    }
+
+
 }
