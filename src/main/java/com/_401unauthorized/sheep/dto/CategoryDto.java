07c087f74191aa4d7351dde2f7f8dc9d96305caa
alsdud0301 <jm931017@gmail.com>
2025-02-24 17:16:28 +0900

    CategoryDto 생성

---
+++ src/main/java/com/_401unauthorized/sheep/dto/CategoryDto.java
@@ -0,0 +1,19 @@
+package com._401unauthorized.sheep.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Accessors(chain = true)
+public class CategoryDto {
+    String major_category;
+    String major_text;
+    String sub_category;
+
+}
