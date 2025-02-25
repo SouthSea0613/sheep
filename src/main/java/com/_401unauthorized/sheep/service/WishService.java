a93843fd6e2c8db07f7ed982783b34dcdcc76777
gsilver-p <wldmsdl6276@naver.com>
2025-02-25 11:30:54 +0900

    2/25일 서비스 스위치케이스 완료

--- src/main/java/com/_401unauthorized/sheep/service/WishService.java
+++ src/main/java/com/_401unauthorized/sheep/service/WishService.java
@@ -33,8 +33,7 @@ public class WishService {
                         wish.setApply_status("견적신청중");
                         break;
                 }
-            }
-            else {
+            } else {
                 wish.setApply_status(null);
             }
         }
@@ -56,7 +55,7 @@ public class WishService {
         wishdto.setWish_money(wishDto.getWish_money());
         wishdto.setWish_addr(wishDto.getWish_addr());
 
-        if(!wishDao.insert_wish(wishdto)) {
+        if (!wishDao.insert_wish(wishdto)) {
             return false;
         }
 
@@ -64,24 +63,45 @@ public class WishService {
 
         for (MajorDto majorDto : wishDto.getMajor_category()) {
             majorDto.setWish_number(wishdto.getWish_number());
-            if(!wishDao.insert_major(majorDto)) {
+            if (!wishDao.insert_major(majorDto)) {
                 return false;
             }
         }
 
         for (SubDto subDto : wishDto.getSub_category()) {
             subDto.setWish_number(wishdto.getWish_number());
-            if(!wishDao.insert_sub(subDto)) {
+            if (!wishDao.insert_sub(subDto)) {
                 return false;
             }
         }
         return true;
     }
-    public List<CategoryListDto> category(Integer wish_number){
-        List<CategoryDto> categoryDto =wishDao.category_detail(wish_number);
+
+    public WishDto essential(Integer wish_number) {
+        WishDto wishDto = wishDao.detail(wish_number);
+            if (wishDto.getWish_type() != null) {
+                switch (wishDto.getWish_type()) {
+                    case "1":
+                        wishDto.setWish_type("아파트");
+                        break;
+
+                    case "2":
+                        wishDto.setWish_type("단독주택");
+                        break;
+
+                    case "3":
+                        wishDto.setWish_type("빌라");
+                        break;
+                }
+            }
+        return wishDto;
+    }
+
+    public List<CategoryListDto> category(Integer wish_number) {
+        List<CategoryDto> categoryDto = wishDao.category_detail(wish_number);
         List<CategoryListDto> categoryListDto = new ArrayList<>();
 
-        for(CategoryDto category : categoryDto){
+        for (CategoryDto category : categoryDto) {
             boolean check = false;
             int tmp = 0;
             if (!categoryListDto.isEmpty()) {
@@ -94,9 +114,193 @@ public class WishService {
                 }
             }
 
+            if (category.getCategory_parent() != null) {
+                switch (category.getCategory_parent()) {
+                    case "1":
+                        category.setCategory_parent("철거");
+                        break;
+
+                    case "2":
+                        category.setCategory_parent("목공");
+                        break;
+
+                    case "3":
+                        category.setCategory_parent("도배");
+                        break;
+
+                    case "4":
+                        category.setCategory_parent("전기");
+                        break;
+
+                    case "5":
+                        category.setCategory_parent("조명");
+                        break;
+
+                    case "6":
+                        category.setCategory_parent("타일");
+                        break;
+
+                    case "7":
+                        category.setCategory_parent("필름");
+                        break;
+
+                    case "8":
+                        category.setCategory_parent("도장");
+                        break;
+
+                    case "9":
+                        category.setCategory_parent("부엌");
+                        break;
+
+                    case "10":
+                        category.setCategory_parent("욕실");
+                        break;
+
+                    case "11":
+                        category.setCategory_parent("수납");
+                        break;
+
+                    case "12":
+                        category.setCategory_parent("바닥");
+                        break;
+
+                    case "13":
+                        category.setCategory_parent("창호");
+                        break;
+
+                    case "14":
+                        category.setCategory_parent("도어");
+                        break;
+
+                    case "15":
+                        category.setCategory_parent("중문");
+                        break;
+
+                    case "16":
+                        category.setCategory_parent("공사준비/마감");
+                        break;
+                }
+            }
+
+            if (category.getCategory_number() != null) {
+                switch (category.getCategory_number()) {
+                    case "17":
+                        category.setCategory_number("단열");
+                        break;
+
+                    case "18":
+                        category.setCategory_number("천장");
+                        break;
+
+                    case "19":
+                        category.setCategory_number("벽");
+                        break;
+
+                    case "20":
+                        category.setCategory_number("현관방화문");
+                        break;
+
+                    case "21":
+                        category.setCategory_number("중문");
+                        break;
+
+                    case "22":
+                        category.setCategory_number("천장몰딩");
+                        break;
+
+                    case "23":
+                        category.setCategory_number("걸레받이몰딩");
+                        break;
+
+                    case "24":
+                        category.setCategory_number("'-'자");
+                        break;
+
+                    case "25":
+                        category.setCategory_number("'ㄱ'자");
+                        break;
+
+                    case "26":
+                        category.setCategory_number("대면형");
+                        break;
+
+                    case "27":
+                        category.setCategory_number("욕조");
+                        break;
+
+                    case "28":
+                        category.setCategory_number("샤워부스");
+                        break;
+
+                    case "29":
+                        category.setCategory_number("드레스룸");
+                        break;
+
+                    case "30":
+                        category.setCategory_number("붙박이장");
+                        break;
+
+                    case "31":
+                        category.setCategory_number("현관장");
+                        break;
+
+                    case "32":
+                        category.setCategory_number("장판");
+                        break;
+
+                    case "33":
+                        category.setCategory_number("강화마루");
+                        break;
+
+                    case "34":
+                        category.setCategory_number("강마루");
+                        break;
+
+                    case "35":
+                        category.setCategory_number("타일");
+                        break;
+
+                    case "36":
+                        category.setCategory_number("일반창");
+                        break;
+
+                    case "37":
+                        category.setCategory_number("터닝도어");
+                        break;
+
+                    case "38":
+                        category.setCategory_number("폴딩도어");
+                        break;
+
+                    case "39":
+                        category.setCategory_number("3연동");
+                        break;
+
+                    case "40":
+                        category.setCategory_number("여닫이");
+                        break;
+
+                    case "41":
+                        category.setCategory_number("슬라이딩");
+                        break;
+
+                    case "42":
+                        category.setCategory_number("입주민동의서");
+                        break;
+
+                    case "43":
+                        category.setCategory_number("EV보양 및 실내외 보양");
+                        break;
+
+                    case "44":
+                        category.setCategory_number("입주청소");
+                        break;
+                }
+            }
+
             if (check) {
                 categoryListDto.get(tmp).getSub_category().add(category.getCategory_number());
-            }else{
+            } else {
                 List<String> ttmmpp = new ArrayList<String>();
                 ttmmpp.add(category.getCategory_number());
 
@@ -108,12 +312,6 @@ public class WishService {
                 categoryListDto.add(tttmmmppp);
             }
         }
-
-
         return categoryListDto;
-    }
-
-    public WishDto essential(Integer wish_number){
-        return wishDao.detail(wish_number);
     }
 }
