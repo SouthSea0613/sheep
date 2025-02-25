980d62d3d73aa59816bc3ebe865d76f26e5daf2b
alsdud0301 <jm931017@gmail.com>
2025-02-25 17:24:16 +0900

    견적 요청 서비스 메서드 작성

--- src/main/java/com/_401unauthorized/sheep/service/TakeoffService.java
+++ src/main/java/com/_401unauthorized/sheep/service/TakeoffService.java
@@ -1,6 +1,7 @@
 package com._401unauthorized.sheep.service;
 
 import com._401unauthorized.sheep.dao.TakeoffDao;
+import com._401unauthorized.sheep.dto.ApplyDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,4 +11,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class TakeoffService {
     private final TakeoffDao takeoffDao;
+
+    public void call(ApplyDto applyDto) {
+      takeoffDao.call(applyDto);
+/*
+        if (applydto.getApply_status().equals("0")) {
+            applydto.setApply_status("진행");
+        }*/
+    }
 }
