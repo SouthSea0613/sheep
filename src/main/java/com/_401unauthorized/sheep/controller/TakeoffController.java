4e87b7827b186718194b37dfabac68fa64f75467
gsilver-p <wldmsdl6276@naver.com>
2025-02-26 19:11:37 +0900

    2/26 견적디테일 작업 중

--- src/main/java/com/_401unauthorized/sheep/controller/TakeoffController.java
+++ src/main/java/com/_401unauthorized/sheep/controller/TakeoffController.java
@@ -1,7 +1,10 @@
 package com._401unauthorized.sheep.controller;
 
 import com._401unauthorized.sheep.dto.ApplyDto;
+import com._401unauthorized.sheep.dto.CategoryListDto;
+import com._401unauthorized.sheep.dto.WishDto;
 import com._401unauthorized.sheep.service.TakeoffService;
+import com._401unauthorized.sheep.service.WishService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,24 +13,43 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/takeoff")
 @Controller
 public class TakeoffController {
     private final TakeoffService takeoffService;
+    private final WishService wishService;
 
     @PostMapping("/call")
     public boolean call(@RequestBody ApplyDto applydto, HttpSession httpSession, RedirectAttributes rttr, Model model) {
         log.info("살려줘");
         log.info(String.valueOf(applydto));
         boolean result = takeoffService.call(applydto);
-        if(result){
+        if (result) {
             log.info("컨트롤러 true");
             return true;
-        }else{
+        } else {
             log.info("컨트롤러 false");
             return false;
+        }
+    }
+
+    @GetMapping("/detail")
+    public String detail(@RequestParam("wish_number") Integer wish_number, Model model) {
+        if (wish_number == null || wish_number < 1) {
+            return "redirect:/list";
+        }
+        WishDto wishDto = wishService.essential(wish_number);
+        List<CategoryListDto> categoryListDtoList = takeoffService.category(wish_number);
+        if (wishDto != null) {
+            model.addAttribute("wishDto", wishDto);
+            model.addAttribute("categoryListDtoList", categoryListDtoList);
+            return "/detail";
+        } else {
+            return "redirect:/list";
         }
     }
 }
--- src/main/java/com/_401unauthorized/sheep/dao/TakeoffDao.java
+++ src/main/java/com/_401unauthorized/sheep/dao/TakeoffDao.java
@@ -1,9 +1,13 @@
 package com._401unauthorized.sheep.dao;
 
 import com._401unauthorized.sheep.dto.ApplyDto;
+import com._401unauthorized.sheep.dto.CategoryDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface TakeoffDao {
     boolean call(ApplyDto applyDto);
+    List<CategoryDto> get_category_detail(Integer wish_number);
 }
