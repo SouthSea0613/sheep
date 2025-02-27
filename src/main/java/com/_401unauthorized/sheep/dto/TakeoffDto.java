8b4fab5c20ca2fb3355a9ca04a35638b1589c672
alsdud0301 <jm931017@gmail.com>
2025-02-27 16:10:39 +0900

    TakeoffDto 생성

---
+++ src/main/java/com/_401unauthorized/sheep/dto/TakeoffDto.java
@@ -0,0 +1,25 @@
+package com._401unauthorized.sheep.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Accessors(chain = true)
+public class TakeoffDto {
+    String user_id;
+    Integer wish_number;
+    String major_category;
+    String major_text;
+    List<String> sub_category;
+    String wish_category_seller_answer;
+    int wish_category_seller_price;
+    String apply_status;
+}
