e5515372d4e055acaec7036487eeaa68e1a98bb4
eunhye0123 <kimun569@gmail.com>
2025-03-06 19:29:40 +0900

    위시에 헤더 안 먹는 거 수정.

--- src/main/resources/static/javascript/write.js
+++ src/main/resources/static/javascript/write.js
@@ -1,17 +1,18 @@
 function toggleUl(){
     console.log("테스트")
     let checkbox = document.querySelectorAll('input[class="major_category_number"]');
-    let listul = document.querySelectorAll("ul")
+    let listdl = document.querySelectorAll("dl")
     console.log(checkbox)
     for (let i = 0; i < checkbox.length; i++) {
         if (checkbox[i].checked) {
-            listul[i].style.display = 'block';
+            listdl[i].style.display = 'block';
+            console.log(listdl[i])
         } else {
-            listul[i].style.display = 'none';
-            listul[i].querySelectorAll('input[type="checkbox"]').forEach(function (innerCheckbox) {
+            listdl[i].style.display = 'none';
+            listdl[i].querySelectorAll('input[type="checkbox"]').forEach(function (innerCheckbox) {
                 innerCheckbox.checked = false;
             });
-            listul[i].querySelectorAll('input[type="text"]').forEach(function (textBox) {
+            listdl[i].querySelectorAll('input[type="text"]').forEach(function (textBox) {
                 textBox.value = '';
             });
         }
