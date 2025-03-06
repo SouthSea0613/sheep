896415e2821ee0ed424f1d880cf167ecda8d0589
yoonsic2 <mrk1251@naver.com>
2025-03-06 14:21:28 +0900

    hi ^^

--- src/main/resources/static/javascript/index.js
+++ src/main/resources/static/javascript/index.js
@@ -42,46 +42,16 @@ function closeOverlay() {
     overlay.setMap(null);
 }
 
-function sample4_execDaumPostcode() {
+function sample6_execDaumPostcode() {
     new daum.Postcode({
-        oncomplete: function(data) {
-            var roadAddr = data.roadAddress;
-            var extraRoadAddr = '';
-
-            if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
-                extraRoadAddr += data.bname;
+        oncomplete : data => {
+            if (data.userSelectedType === 'R') {
+                $("#sample6_address").val(data.roadAddress);
             }
-            if(data.buildingName !== '' && data.apartment === 'Y'){
-                extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+            else {
+                $("#sample6_address").val(data.jibunAddress);
             }
-            if(extraRoadAddr !== ''){
-                extraRoadAddr = ' (' + extraRoadAddr + ')';
-            }
-
-            document.getElementById('sample4_postcode').value = data.zonecode;
-            document.getElementById("sample4_roadAddress").value = roadAddr;
-            document.getElementById("sample4_jibunAddress").value = data.jibunAddress;
-
-            if(roadAddr !== ''){
-                document.getElementById("sample4_extraAddress").value = extraRoadAddr;
-            } else {
-                document.getElementById("sample4_extraAddress").value = '';
-            }
-
-            var guideTextBox = document.getElementById("guide");
-            if(data.autoRoadAddress) {
-                var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
-                guideTextBox.innerHTML = '(예상 도로명 주소 : ' + expRoadAddr + ')';
-                guideTextBox.style.display = 'block';
-
-            } else if(data.autoJibunAddress) {
-                var expJibunAddr = data.autoJibunAddress;
-                guideTextBox.innerHTML = '(예상 지번 주소 : ' + expJibunAddr + ')';
-                guideTextBox.style.display = 'block';
-            } else {
-                guideTextBox.innerHTML = '';
-                guideTextBox.style.display = 'none';
-            }
+            $("#sample6_detailAddress").focus();
         }
     }).open();
 }
--- src/main/resources/static/javascript/user.js
+++ src/main/resources/static/javascript/user.js
@@ -93,10 +93,10 @@ function join_check() {
 		}
 	}
 
-	const road_address = $('#sample4_roadAddress');
-	const detail_address = $('#sample4_detailAddress');
+	const address = $('#sample6_address');
+	const detail_address = $('#sample6_detailAddress');
 	const  user_addr = $('#user_addr');
-	user_addr.val(road_address.val() +' '+ detail_address.val());
+	user_addr.val(address.val() +'@'+ detail_address.val());
 
 	$('#join_form').submit();
 }
@@ -183,44 +183,14 @@ function login() {
 
 function find_addr(){
 	new daum.Postcode({
-		oncomplete: function(data) {
-			var roadAddr = data.roadAddress;
-			var extraRoadAddr = '';
-
-			if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
-				extraRoadAddr += data.bname;
+		oncomplete : data => {
+			if (data.userSelectedType === 'R') {
+				$("#sample6_address").val(data.roadAddress);
 			}
-			if(data.buildingName !== '' && data.apartment === 'Y'){
-				extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+			else {
+				$("#sample6_address").val(data.jibunAddress);
 			}
-			if(extraRoadAddr !== ''){
-				extraRoadAddr = ' (' + extraRoadAddr + ')';
-			}
-
-			document.getElementById('sample4_postcode').value = data.zonecode;
-			document.getElementById("sample4_roadAddress").value = roadAddr;
-			document.getElementById("sample4_jibunAddress").value = data.jibunAddress;
-
-			if(roadAddr !== ''){
-				document.getElementById("sample4_extraAddress").value = extraRoadAddr;
-			} else {
-				document.getElementById("sample4_extraAddress").value = '';
-			}
-
-			var guideTextBox = document.getElementById("guide");
-			if(data.autoRoadAddress) {
-				var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
-				guideTextBox.innerHTML = '(예상 도로명 주소 : ' + expRoadAddr + ')';
-				guideTextBox.style.display = 'block';
-
-			} else if(data.autoJibunAddress) {
-				var expJibunAddr = data.autoJibunAddress;
-				guideTextBox.innerHTML = '(예상 지번 주소 : ' + expJibunAddr + ')';
-				guideTextBox.style.display = 'block';
-			} else {
-				guideTextBox.innerHTML = '';
-				guideTextBox.style.display = 'none';
-			}
+			$("#sample6_detailAddress").focus();
 		}
 	}).open();
 }
