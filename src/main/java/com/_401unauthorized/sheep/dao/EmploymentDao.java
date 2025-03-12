db350ebd8b46a9ec6099af4462c07eee7e4a5066
gsilver-p <wldmsdl6276@naver.com>
2025-03-12 17:21:17 +0900

    3/12 구인구직 글쓰기에 지역설정 추가 예정!

--- src/main/java/com/_401unauthorized/sheep/dao/EmploymentDao.java
+++ src/main/java/com/_401unauthorized/sheep/dao/EmploymentDao.java
@@ -3,8 +3,11 @@ package com._401unauthorized.sheep.dao;
 import com._401unauthorized.sheep.dto.BoardDto;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface EmploymentDao {
     boolean write(BoardDto employmentDto);
-
+    void insert_count(Integer board_number,Integer job_count);
+    List<BoardDto> list(BoardDto boardDto);
 }
--- src/main/java/com/_401unauthorized/sheep/service/EmploymentService.java
+++ src/main/java/com/_401unauthorized/sheep/service/EmploymentService.java
@@ -7,15 +7,45 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class EmploymentService {
     public final Integer list_count = 10;
+    public final Integer page_count = 2;
     private final EmploymentDao employmentDao;
 
-    public boolean write(BoardDto employmentDto) {
+    public boolean write(BoardDto boardDto) {
+        if (employmentDao.write(boardDto)) {
+            log.info("boardDto = {}", boardDto.getBoard_number());
+            int board_number = boardDto.getBoard_number();
+            int job_count = boardDto.getJob_count();
+            log.info("job count: {}", job_count);
+            employmentDao.insert_count(board_number,job_count);
+            return true;
+        }
         return false;
     }
 
+    public List<BoardDto> list(BoardDto boardDto) {
+       Integer page_number = boardDto.getPage_number();
+       boardDto.setStart_index((page_number - 1) * boardDto.getList_count());
+       return employmentDao.list(boardDto);
+    }
+//
+//    public String paging(BoardDto boardDto) {
+//        int total_number = employmentDao.count(boardDto);
+//        String url = null;
+//        if(boardDto.getColname() != null) {
+//            url = "/employment?colname=" + boardDto.getColname() + "&keyword=" + boardDto.getKeyword() + "&";
+//        } else {
+//            url = "/employment?";
+//        }
+//
+//        Paging paging = new Paging(total_number, boardDto.getPage_number(), list_count, page_count, url);
+//    }
 }
