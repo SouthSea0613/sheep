87ea407a379995596984074c47d2801f53be5679
South_Sea13 <tksz0613@gmail.com>
2025-03-17 22:15:17 +0900

    what's happened's happened

---
+++ src/main/java/com/_401unauthorized/sheep/controller/SellerController.java
@@ -0,0 +1,26 @@
+package com._401unauthorized.sheep.controller;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/seller")
+@Controller
+public class SellerController {
+    @GetMapping("/main")
+    public String main(HttpSession httpSession, Model model) {
+        return "/seller/main";
+    }
+
+    @PostMapping("/write")
+    public String write(HttpSession httpSession, Model model) {
+        return "/seller/write";
+    }
+}
